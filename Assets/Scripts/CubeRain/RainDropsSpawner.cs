ce7a743d1658029d79fbfab8d7e08e517034d2bd
Gleb <keniomg77798@gmail.com>
2024-06-20 15:26:03 +0400

    1

--- Assets/Scripts/CubeRain/RainDropsSpawner.cs
+++ Assets/Scripts/CubeRain/RainDropsSpawner.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class RainDropsSpawner : MonoBehaviour
 {
     [SerializeField] private RainDropObject _rainDropObject;
+    [SerializeField] private int _poolCapacity;
+    [SerializeField] private int _poolMaxSize;
 
+    private ObjectPool<RainDropObject> _pool;
     private float _widthSpawnArea;
     private float _lengthSpawnArea;
 
@@ -13,30 +17,45 @@ public class RainDropsSpawner : MonoBehaviour
         int numbersOfSide = 2;
         _widthSpawnArea = transform.localScale.x / numbersOfSide;
         _lengthSpawnArea = transform.localScale.z / numbersOfSide;
+        _pool = new ObjectPool<RainDropObject>(
+        createFunc: () => Instantiate(_rainDropObject),
+        actionOnGet: (rainDropObject) => ActionOnGet(rainDropObject),
+        actionOnRelease: (rainDropObject) => ActionOnRelease(rainDropObject),
+        actionOnDestroy: (rainDropObject) => Destroy(rainDropObject),
+        collectionCheck: true,
+        defaultCapacity: _poolCapacity,
+        maxSize: _poolMaxSize);
     }
 
-    private void Start()
+    private void ActionOnRelease(RainDropObject rainDropObject)
     {
-        StartCoroutine(SpawnRainDrops());
+        rainDropObject.gameObject.SetActive(false);
+
     }
 
-    private void InstantiateRainDrop()
+    private void ActionOnGet(RainDropObject rainDropObject)
     {
         float xSpawnPosition = Random.Range(transform.position.x - _widthSpawnArea, transform.position.x + _widthSpawnArea);
         float ySpawnPosition = transform.position.y;
         float zSpawnPosition = Random.Range(transform.position.z - _lengthSpawnArea, transform.position.z + _lengthSpawnArea);
         Vector3 spawnPosition = new Vector3(xSpawnPosition, ySpawnPosition, zSpawnPosition);
         Quaternion rotationPosition = Quaternion.Euler(0, 0, 0);
-        Instantiate(_rainDropObject, spawnPosition, rotationPosition);
+        rainDropObject.transform.position = spawnPosition;
+        rainDropObject.transform.rotation = rotationPosition;
+
+        rainDropObject.TryGetComponent(out Rigidbody rigidbody);
+        rigidbody.velocity = Vector3.zero;
+        rainDropObject.gameObject.SetActive(true);
     }
 
-    private IEnumerator SpawnRainDrops()
+    private void GetObject()
     {
-        while (true)
-        {
-            float delayValue = 1;
-            InstantiateRainDrop();
-            yield return new WaitForSeconds(delayValue);
-        }
+        _pool.Get();
+    }
+
+    private void Start()
+    {
+        float spawnDelayValue = 1;
+        InvokeRepeating(nameof(GetObject), 0.0f, spawnDelayValue);
     }
 }
